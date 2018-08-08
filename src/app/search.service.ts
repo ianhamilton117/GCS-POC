0a9e7bab41d383b19301577ed637105570750f55
Ian Hamilton <ian.hamilton117@gmail.com>
2018-08-08 11:23:10 -0700

    Refactoring search to handle url parameters more cleanly

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SearchResult } from './search/search-result';
 import { GCSResponse } from './search/gcs-response';
+import { RestURLBuilder } from 'rest-url-builder';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,21 @@ export class SearchService {
 
   searchResults: SearchResult[] = [];
 
+  private urlBuilder = new RestURLBuilder();
+
   constructor(private httpClient: HttpClient) { }
 
-  search(query: String): SearchResult[] {
+  search(query: string): SearchResult[] {
     this.searchResults = [];
-    let url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDkGQ2TErpTl--zkgGErCh0_XhUFIWQC54&cx=014722161919417917553:vmkwzgw4l5u&q=";
-    // let key = 
-    this.httpClient.get<GCSResponse>(url + query)
+    let url = "https://www.googleapis.com/customsearch/v1?key=:key&cx=:cx&q=:q" //Format for REST URL
+    let key = "AIzaSyDkGQ2TErpTl--zkgGErCh0_XhUFIWQC54"; //Google API key
+    let cx = "014722161919417917553:vmkwzgw4l5u"; //Google search engine ID
+    let builder = this.urlBuilder.buildRestURL(url);
+    builder.setQueryParameter('key', key);
+    builder.setQueryParameter('cx', cx);
+    builder.setQueryParameter('q', query);
+    let finalURL = builder.get();
+    this.httpClient.get<GCSResponse>(finalURL)
     .subscribe(
       (response) => {
         for (var i = 0; i < response.items.length; i++) {
