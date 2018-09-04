ca190efb4ab288a05e802b75c4a23273baa57020
Ian Hamilton <ian.hamilton117@gmail.com>
2018-09-04 16:47:29 -0700

    Minor code tweaks

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -12,17 +12,18 @@ export class SearchService {
   constructor(private httpClient: HttpClient) { }
 
   search(query: string, pageNum: number, reqWords?: string, reqPhrase?: string, exclWords?: string, orTerms?: string, fileType?: string): SearchResults {
-    const SEARCH_RESULTS_PER_PAGE = 10; //10 is the default number of results per page. It is also the maximum.
-    let searchResults = new SearchResults([], 0, 0);
-    let urlBuilder = new RestURLBuilder();
+    const resultsPerPage = 10; //10 is the default number of results per page returned by the Google API. It is also the maximum.
+    const searchResults = new SearchResults([], 0, 0);
+    const urlBuilder = new RestURLBuilder();
 
-    let url = "https://www.googleapis.com/customsearch/v1?key=:key&cx=:cx&q=:q&hq=:hq&exactTerms=:exactTerms&excludeTerms=:excludeTerms&orTerms=:orTerms&fileType=:fileType&start=:start" //Format for REST URL
-    let key = "AIzaSyBlPeSIOgT_wPjyjWFnqTW3l9L2A3ZY9GA"; //Google API key
-    let cx = "001477481991178287809:r-wouyqyxew"; //Google search engine ID
-    let startIndex = String(SEARCH_RESULTS_PER_PAGE * (pageNum - 1) + 1) //This calculates which result will be the first result on a given page. 
-                                                                         // 1 for page 1, 11 for page 2, 22 for page 3, etc.
+    const url = "https://www.googleapis.com/customsearch/v1?key=:key&cx=:cx&q=:q&hq=:hq&exactTerms=:exactTerms&excludeTerms=:excludeTerms&orTerms=:orTerms&fileType=:fileType&start=:start" //Format for REST URL
+    const key = "AIzaSyBlPeSIOgT_wPjyjWFnqTW3l9L2A3ZY9GA"; //Google API key
+    const cx = "001477481991178287809:r-wouyqyxew"; //Google search engine ID
 
-    let builder = urlBuilder.buildRestURL(url);
+    //Calculate which result will be the first result on a given page. 1 for page 1, 11 for page 2, 22 for page 3, etc.
+    const startIndex = String(resultsPerPage * (pageNum - 1) + 1)
+
+    const builder = urlBuilder.buildRestURL(url);
     builder.setQueryParameter('key', key);
     builder.setQueryParameter('cx', cx);
     builder.setQueryParameter('q', query);
@@ -43,14 +44,14 @@ export class SearchService {
     this.httpClient.get<GCSResponse>(finalURL)
     .subscribe(
       (response) => {
-        let numResults = response.queries.request[0].totalResults;
+        const numResults = response.queries.request[0].totalResults;
         if (numResults == 0) return searchResults;
 
         //Calculate the number of pages required to fit all results, with a max of 100 results (Google won't return any results past 100)
-        let numPages = Math.floor((Math.min(numResults, 100) - 1) / SEARCH_RESULTS_PER_PAGE) + 1;
+        const numPages = Math.floor((Math.min(numResults, 100) - 1) / resultsPerPage) + 1;
 
         response.items.forEach(item => {
-          let searchResult = new SearchResult(item.htmlTitle, item.htmlSnippet, item.link, item.displayLink);
+          const searchResult = new SearchResult(item.htmlTitle, item.htmlSnippet, item.link, item.displayLink);
           searchResults.results.push(searchResult);
         });
 
